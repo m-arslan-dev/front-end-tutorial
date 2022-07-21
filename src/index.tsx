71726c9a814c142ca2c0b560524f3922a87b74ee
Muhammad Arslan <m.arslan@ssidecisions.com>
2022-07-21 15:51:04 +0500

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const unused = "something";
+
+const App = () => (
+	<h1>
+		My React {unused} and TypeScript App!! {new Date().toLocaleDateString()}
+	</h1>
+);
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
