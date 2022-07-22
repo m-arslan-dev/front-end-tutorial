98b09264e627e71d5b417c0288e25f456c0cf4ce
Talha <talhaabinnasir@gmail.com>
2022-07-22 10:56:21 +0500

    ant design

--- tableapp/src/App/Components/Content.jsx
+++ tableapp/src/App/Components/Content.jsx
@@ -1,100 +1,122 @@
 import React from 'react'
 import { useRef, useContext, useState } from "react";
 import "../../App.css";
+import 'antd/dist/antd.css';
 import {contextData} from "../Context/ContextApi";
-import { Button, Modal } from 'antd';
+import { Button, Modal, Space, Table } from 'antd';
 
 function Content() {
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
+const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
+const showModal = () => {
+  setIsModalVisible(true);
+};
 
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
+const handleOk = () => {
+  setIsModalVisible(false);
+};
 
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
+const handleCancel = () => {
+  setIsModalVisible(false);
+};
 
-  const {state, dispatch } = useContext(contextData);
+const {state, dispatch } = useContext(contextData);
 
-  const rolln = useRef();
-  const namee = useRef();
-  const dept = useRef();
+const rolln = useRef();
+const namee = useRef();
+const dept = useRef();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (rolln.current.value != '' && namee.current.value != '' && dept.current.value != ''){
-    dispatch({
-      type: 'add',
-      rollno: rolln.current.value,
-      name: namee.current.value,
-      department: dept.current.value
-    });
-    rolln.current.value='';
-    namee.current.value='';
-    dept.current.value='';
-  }
+const handleSubmit = (event) => {
+  event.preventDefault();
+  if (rolln.current.value != '' && namee.current.value != '' && dept.current.value != ''){
+  dispatch({
+    type: 'add',
+    rollno: rolln.current.value,
+    name: namee.current.value,
+    department: dept.current.value
+  });
+  rolln.current.value='';
+  namee.current.value='';
+  dept.current.value='';
+  setIsModalVisible(false);
+}
 }
 
-  return (
+const columns = [
+  {
+    title: 'Roll No.',
+    dataIndex: 'rollno',
+    key: 'rollno',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Department',
+    dataIndex: 'department',
+    key: 'department',
+    render: (text) => <a>{text}</a>,
+  }
+];
+
+return (
+  <div>
+    <Table columns={columns} dataSource={state}></Table>
     <div>
-      <div>
-      <table className='center'>
-          <th>Roll No.</th>
-          <th>Name</th>
-          <th>Department</th>
-      {state.map((item, i) => (
-          <tr key={i}>
-              <td>{item.rollno}</td>
-              <td>{item.name }</td>
-              <td>{item.department }</td>
-              <button className='Del-btn' onClick={() => dispatch({ type: 'remove', i })}> REMOVE </button>
-          </tr>
-          ))}
-        </table>
-      </div>
-      <div className='margin-div'>
-      <label>Enter your Roll Number
-      <input 
-        ref={rolln}
-        type="text" 
-      />
-      </label>
-      <label>Enter your Name
-      <input 
-        ref={namee}
-        type="text" 
-      />
-      </label>
-        <label>Enter your Department
-        <input 
-          ref={dept}
-          type="text" 
-        />
-        </label>
-        </div>
-        <div className='margin-div'>
-    <button className='Add-btn' onClick = {handleSubmit}>
-  ADD
-</button>
-</div>
-<div>
-      <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
-      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal>
+    <table className='center'>
+        <th>Roll No.</th>
+        <th>Name</th>
+        <th>Department</th>
+    {state.map((item, i) => (
+        <tr key={i}>
+            <td>{item.rollno}</td>
+            <td>{item.name }</td>
+            <td>{item.department }</td>
+            <button className='Del-btn' onClick={() => dispatch({ type: 'remove', i })}> REMOVE </button>
+        </tr>
+        ))}
+      </table>
     </div>
+<div className='margin-div'>
+    <Button type="primary" onClick={showModal}>
+      Add Student
+    </Button>
+    <Modal title="Basic Modal" visible={isModalVisible} onOk={handleSubmit} onCancel={handleCancel}>
+      <div className='grid-container'>
+        <div className='grid-item'>
+    <label className='label-margin'>Enter your Roll Number</label>
+    <input 
+      ref={rolln}
+      type="text" 
+    />
+    </div>
+
+    <br/>
+    <div className='grid-container'>
+    <label className='label-margin'>Enter your Name</label>
+    <input 
+      ref={namee}
+      type="text" 
+    />
+    </div>
+    <br/>
+    <div className='grid-container'>
+    <label className='label-margin'>Enter your Department</label>
+      <input 
+        ref={dept}
+        type="text" 
+      />
+      </div>
+      </div>
+    </Modal>
   </div>
-  );
+</div>
+);
 }
 
 export default Content
--- tableapp/src/App/Components/Header.jsx
+++ tableapp/src/App/Components/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 
 function Header(props) {
   return (
     <div>
-      <h1>Header</h1>
+      <Menu mode="horizontal" defaultSelectedKeys={['mail']}>
+    <Menu.Item key="mail" icon={<MailOutlined />}>
+      Home
+    </Menu.Item>
+    <Menu.SubMenu key="SubMenu" title="Menu" icon={<SettingOutlined />}>
+      <Menu.Item key="two" icon={<AppstoreOutlined />}>
+      Navigation Two
+      </Menu.Item>
+      <Menu.Item key="three" icon={<AppstoreOutlined />}>
+        Navigation Three
+      </Menu.Item>
+      <Menu.ItemGroup title="Item Group">
+        <Menu.Item key="four" icon={<AppstoreOutlined />}>
+          Navigation Four
+        </Menu.Item>
+        <Menu.Item key="five" icon={<AppstoreOutlined />}>
+          Navigation Five
+        </Menu.Item>
+      </Menu.ItemGroup>
+    </Menu.SubMenu>
+  </Menu>
     </div>
   )
 }
