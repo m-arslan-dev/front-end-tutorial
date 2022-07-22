d15e586fd32b7eca94ae79c639a4116993c6297e
Talha <talhaabinnasir@gmail.com>
2022-07-22 16:26:36 +0500

    Done

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index1.scss';
 
-const unused = "something";
+const unused = 'something';
 
 const App = () => (
-	<h1>
-		My React {unused} and TypeScript App!! {new Date().toLocaleDateString()}
-	</h1>
+  <h1>
+    My React {unused} and TypeScript App!! {new Date().toLocaleDateString()}
+  </h1>
 );
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
